85e5d619226066850e00588463f4c5c67adba566
Mark Uy <macmac.uy@gmail.com>
2025-04-18 15:34:51 +0800

    fix mocks

--- src/mocks/Recipes.ts
+++ src/mocks/Recipes.ts
@@ -5,7 +5,9 @@ const createRecipe = () => {
     id: faker.string.uuid(),
     title: faker.food.dish(),
     description: faker.lorem.paragraphs(5),
+    instructions: faker.food.description(),
     name: faker.person.fullName(),
+    email: faker.internet.email(),
     image: `https://picsum.photos/id/${faker.helpers.rangeToNumber({
       min: 1,
       max: 100,
